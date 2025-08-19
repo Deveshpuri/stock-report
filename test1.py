d62d91e92af54fef039f6986b763c5a2e05b7b96
Devesh Puri <deveshgoswmai191@gmail.com>
2025-08-19 12:18:48 +0530

    to freeze only colum b and c

--- test1.py
+++ test1.py
@@ -26,6 +26,7 @@ ist = pytz.timezone('Asia/Kolkata')
 start_time = time.time()
 logger.info("Starting stock report script...")
 
+
 # Step 1: Execute Google Apps Script macro twice with a 4-second pause
 macro_url = "https://script.google.com/macros/s/AKfycbxhQaMwCqxL6m-RkjtIzm-bjz1Q3YiI4kEiI0_5TLaXBbGxLDHrKnW5FJIA-_8CR8DFCw/exec"
 logger.info("Executing first Google Apps Script macro...")
